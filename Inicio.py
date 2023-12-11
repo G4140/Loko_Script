63bfe68587ce6dfcb74946edc488109495ca70c1
DiCaprio <152615917+G4140@users.noreply.github.com>
2023-12-11 09:43:51 -0300

    Create Inicio.py

---
+++ Inicio.py
@@ -0,0 +1,8 @@
+lista = ({1},{2},{3},{4},{5})
+Nomes = ("João",20,"Pedro",30,"Thiago",40,"No Barquinho",50,"")
+class LOKOScript():
+    def __init__int():
+        if 1>="João":
+            print ("Hellow João",lista)
+            
+        
